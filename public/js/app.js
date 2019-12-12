348197bc5b089ed10ef21064afc22bd8aa7b64dd
Asleepless Knight <41958127+ElementalXriath@users.noreply.github.com>
2019-12-11 19:35:50 -0500

    Working on it

--- public/js/app.js
+++ public/js/app.js
@@ -4,21 +4,32 @@
 
 const weatherForm = document.querySelector('#form');
 const search = document.querySelector('#search');
-const forecastMess = document.querySelector('#forecast');
+const timeMess = document.querySelector('#time');
+const f_rain = document.querySelector('#f-rain');
+const f_location = document.querySelector('#f_location');
+
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value;
 
-    forecastMess.textContent = 'Loading'
+    timeMess.textContent = 'Loading'
+    f_rain.textContent = 'Loading'
 
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (!data) {
             console.log('error')
         } else {
-            forecastMess.textContent = data.location
+            f_location.textContent = data.location
+            timeMess.textContent =  dateTime
+            f_rain.textContent = data.forecast.currentTemp + ' F '
         }
         
        })
